ac3bc5112a43fff9fbfa4ccfc5485e26ec518f71
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2019-10-09 16:16:37 +0300

    Updates

---
+++ precache-manifest.328ad4e61c4f3aa461d671e8ddb4078a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "004fab36af453f33e5989c6502267096",
+    "url": "/chess-react/index.html"
+  },
+  {
+    "revision": "846f2adacbf73cdf55eb",
+    "url": "/chess-react/static/css/main.d5ae42a2.chunk.css"
+  },
+  {
+    "revision": "3f0e7bf008670d903da9",
+    "url": "/chess-react/static/js/2.96ee42ae.chunk.js"
+  },
+  {
+    "revision": "846f2adacbf73cdf55eb",
+    "url": "/chess-react/static/js/main.28c9d67c.chunk.js"
+  },
+  {
+    "revision": "0bf2507389356c8eea76",
+    "url": "/chess-react/static/js/runtime-main.2948af1c.js"
+  }
+]);
